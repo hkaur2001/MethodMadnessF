17a78695819ccdd52ae4daf483e851f5a66c206f
computweryi.li <hkaur0745@bths.edu>
2018-10-02 14:47:57 -0400

    Oct 2 update

--- src/HKaurLib.java
+++ src/HKaurLib.java
@@ -1,2 +1,32 @@
 public class HKaurLib {
+    public static boolean isPrime(int n)
+    {
+        for (int i = 2; i <n; i++)
+        {
+            if (n%i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void isPalindrome(int n)
+    {
+        int r,sum=0,temp;
+        temp=n;
+        while(n>0){
+            r=n%10;  //gets remainder
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(temp==sum)
+            System.out.println("palindrome number ");
+        else
+            System.out.println("not palindrome");
+    }
+
+
 }
+
+
--- src/TestFile.java
+++ src/TestFile.java
@@ -1,2 +1,9 @@
 public class TestFile {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Method Madness");
+        System.out.println(HKaurLib.isPrime(10));
+        System.out.println(HKaurLib.isPrime(7));
+    }
 }
