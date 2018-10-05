77fee7a7364fdd6e69654de07f597ca4b1a5b6d5
Harpreet Kaur <hkaur0745@bths.edu>
2018-10-05 05:26:14 -0400

    oct 5 update

--- src/HKaurLib.java
+++ src/HKaurLib.java
@@ -1,16 +1,4 @@
 public class HKaurLib {
-    public static boolean isPrime(int n)
-    {
-        for (int i = 2; i <n; i++)
-        {
-            if (n%i == 0)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void isPalindrome(int n)
     {
         int r,sum=0,temp;
@@ -26,7 +14,33 @@ public class HKaurLib {
             System.out.println("not palindrome");
     }
 
+    public static void (int num1, int num2, int num 3,)  //add the name leastCommonMultiple
+    {
+    int lcm;
 
+        // maximum number between n1 and n2 is stored in lcm
+        if (num1>num2){lcm = num1;
+        }else{
+            lcm = num2;
+        }
+        // Always true
+        while(true)
+        {
+            if( lcm % num1 == 0 && lcm % num2 == 0 )
+            {
+                System.out.printf("The LCM of %d and %d is %d.", num1, num2, lcm);
+                break;
+            }
+            ++lcm;
+        }
+    }
+
+    public static void quadSolver (Double a, Double b, Double c){
+    if (b**2 - 4*a*c > 0){
+    return "The roots of the quadratic equation are real and unequal";}
+    else{
+    if (b**2- 4*a*c<0){
+    return "The roots of the quadratic equation are unequal and not real.  These values are imaginary because they result in a negative value under the square root when calculating the discriminant";
+    else if (b**2- 4*a*c=0)
+    return "The roots of the quadratic equation are real and equal";}
 }
-
-
--- src/TestFile.java
+++ src/TestFile.java
@@ -1,9 +1,9 @@
 public class TestFile {
 
-    public static void main(String[] args)
+    public static int void(String[] args)
     {
         System.out.println("Hello Method Madness");
-        System.out.println(HKaurLib.isPrime(10));
-        System.out.println(HKaurLib.isPrime(7));
+        System.out.println(HKaurLib.isPalindrome(101));
+        System.out.println(HKaurLib.isPalindrome(723));
     }
 }
