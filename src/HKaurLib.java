5eb5357f1e6bde0fc9ef54daaf0118ea8390dd09
Harpreet Kaur <hkaur0745@bths.edu>
2018-10-07 23:22:01 -0400

    latest update oct 7

--- src/HKaurLib.java
+++ src/HKaurLib.java
@@ -1,4 +1,9 @@
 public class HKaurLib {
+    /**
+     *Checks a string to see if it is the same backwards and forwards
+     * @param str The string that is inputted to be checked
+     * @return returns true if it is a palindrome and false if it is not
+     */
     public static boolean isPalindrome(String str) {
         if (str.length() <= 1) {
             return true;
@@ -9,6 +14,11 @@ public class HKaurLib {
                 && isPalindrome(str.substring(1, str.length() - 1));
     }
 
+    /**
+     *Changes the format of a date in mm/dd/yyyy format to dd – mm - yyyy format
+     * @param date the date entered in mm/dd/yyyy format
+     * @return Returns the dd-mm-yyyy format
+     */
     public static String dateStr(String date){
         String day = date.substring(3,5);
         String month = date.substring(0,2);
@@ -17,7 +27,13 @@ public class HKaurLib {
         return Date;
     }
 
-
+    /**
+     * Finds the least common multiple of three integers
+     * @param num1 first inputted integer
+     * @param num2 second  integer
+     * @param num3 third integer
+     * @return the least common multiple of the three inputs
+     */
     public static int leastCommonMultiple(int num1, int num2, int num3){
         int i = 1;
         while((i%num1!=0)||(i%num2!=0)||(i%num3!=0)){
@@ -26,6 +42,11 @@ public class HKaurLib {
         return i;
     }
 
+    /**
+     * Sums up all integers up to entered value
+     * @param integer the number to sum up to
+     * @return returns the sum of the numbers from 0 to the input
+     */
     public static String sumUpTo(int integer) {
             int sum = 0;
             for(int i = 1;i <= integer;i++)
@@ -36,6 +57,11 @@ public class HKaurLib {
         return "";
     }
 
+    /**
+     * Determines if an integer is prime
+     * @param n an integer
+     * @return returns true if it's a prime number and false  if it is not
+     */
     public static boolean isPrime(int n){
         for (int i = 2; i <=Math.sqrt(n) ; i++) {
             if(n%i==0)
@@ -43,6 +69,12 @@ public class HKaurLib {
         }
         return true;
     }
+
+    /**
+     * Prints list of prime numbers making use of is prime function above
+     * @param num a positive integer and the number of prime numbers to print
+     * @return returns the first num prime numbers
+     */
     public static String primePrinter(int num){
         int number = 2;
         int primeCount = 0;
@@ -57,6 +89,13 @@ public class HKaurLib {
         return "";
     }
 
+    /**
+     * Makes use of the discriminant and quadratic formula to find roots
+     * @param a a real number representing the a value of a quadratic equation
+     * @param b real number representing b value of quadratic equation
+     * @param c real number reprsenting c value of quadratic equation
+     * @return returns the type of roots and if they exist, the actual roots of equation
+     */
     public static String quadSolver(Double a, Double b, Double c) {
         if (Math.pow(b, 2) - 4 * a * c > 0) {
             double x1;
@@ -86,6 +125,12 @@ public class HKaurLib {
         return null;
     }
 
+    /**
+     * Implementing the Caesar Cipher to use in Vigenere Cipher below
+     * @param character the letters of the alphabet string
+     * @param shift the amount of characters moved
+     * @return returns the shifted character
+     */
     public static String ccipher (String character, int shift){
         String alpha = "abcdefghijklmnopqrstuvwxyz";
         int index = alpha.indexOf(character);
@@ -94,7 +139,12 @@ public class HKaurLib {
         return character;
     }
 
-
+    /**
+     * Uses Vigenere Cipher to encrypt inputted string
+     * @param message The string that is being encrypted
+     * @param key The key used to encrypt
+     * @return The encrypted message.
+     */
     public static String vigCipher(String message, String key){
         String alpha = "abcdefghijklmnopqrstuvwxyz";
         String encrypt = "";
